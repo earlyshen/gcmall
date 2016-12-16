9b4c5b11a280684f54fc352e0dbd06ca14e28241
Erh-Li Shen <erh-li.shen@workday.com>
2016-12-16 11:01:00 -0800

    Add gcmall.rb & cards.csv

---
+++ gcmall.rb
@@ -0,0 +1,35 @@
+#!/usr/bin/env ruby
+
+require 'CSV'
+
+def getBalance(card, month, year, cvv)
+  url = 'https://mygift.giftcardmall.com/Card/Login?returnUrl=Transactions'
+  `curl -sS -c cookie --data "CardNumber=#{card}&ExpirationMonth=#{month}&ExpirationYear=#{year}&SecurityCode=#{cvv}" #{url}`
+  response = `curl -sS -b cookie -H "Referer: #{url}" 'https://mygift.giftcardmall.com/Card/Transactions'`
+  matches = response.scan(/<\/h6><h5>([\d\.\$]+)<\/h5><\/td>/i)
+  if matches.count == 3
+    last4 = matches[0][0]
+    available = matches[1][0]
+    initial = matches[2][0]
+    return last4, available, initial
+  else
+    return nil, nil, nil
+  end
+
+end
+
+printf("%13s %10s %8s\n",'Last 4 Digit', 'Available', 'Initial')
+print "====================================\n"
+
+CSV.foreach("cards.csv", { headers: true, header_converters: :symbol }) do |row|
+  card = row.to_hash
+  last4, available, initial = getBalance(card[:card_number], card[:month], card[:year], card[:cvv])
+  if last4
+    printf("%13s %10s %8s\n", last4, available, initial)
+  else
+    last4 = card[:card_number][-4,4]
+    printf("%13s %19s\n", last4, 'Card not found')
+  end
+end
+
+print "====================================\n"
